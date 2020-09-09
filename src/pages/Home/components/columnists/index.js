3f1af248f74048a08df78a22700ee3ebb480da57
Fábio Torres de Quadros <ftquadros@positivo.com.br>
2020-09-09 19:17:02 -0300

    ajustes estilização colunistas

--- src/pages/Home/components/columnists/index.js
+++ src/pages/Home/components/columnists/index.js
@@ -7,52 +7,52 @@ import MinhaPlaylist from "../../../../assets/minha-playlist-2.png";
 import OnTheRocks from "../../../../assets/on-the-rocks-home.png";
 import SetimaArte from "../../../../assets/setima-arte.png";
 import TechGeek from "../../../../assets/techgeek.png";
-import { Row } from 'react-flexbox-grid';
-import { ColunaContainer, LogoRow } from './styled';
-import Shuffle from '../../../../util/shuffleArray';
+import { Row } from "react-flexbox-grid";
+import { Container, ColunaContainer, LogoRow } from "./styled";
+import Shuffle from "../../../../util/shuffleArray";
 
 const Columnists = () => {
   const [colunistas, setColunistas] = useState([]);
 
-   useEffect(() => {
+  useEffect(() => {
     const buildColunistas = (data) => {
-        let result = [];
-    
-        data.forEach((element) => {
-          let image = undefined;
-          switch (element.name) {
-            case "Gostosuras da Gi":
-              image = GostosurasDaGi;
-              break;
-            case "Maternagem":
-              image = Maternagem;
-              break;
-            case "Minha Playlist":
-              image = MinhaPlaylist;
-              break;
-            case "On The Rocks":
-              image = OnTheRocks;
-              break;
-            case "Sétima Arte":
-              image = SetimaArte;
-              break;
-            case "TechGeek":
-              image = TechGeek;
-              break;
-            default:
-              break;
-          }
-    
-          result.push({
-            id: element.id,
-            name: element.name,
-            logo: image,
-            link: element.link,
-          });
+      let result = [];
+
+      data.forEach((element) => {
+        let image = undefined;
+        switch (element.name) {
+          case "Gostosuras da Gi":
+            image = GostosurasDaGi;
+            break;
+          case "Maternagem":
+            image = Maternagem;
+            break;
+          case "Minha Playlist":
+            image = MinhaPlaylist;
+            break;
+          case "On The Rocks":
+            image = OnTheRocks;
+            break;
+          case "Sétima Arte":
+            image = SetimaArte;
+            break;
+          case "TechGeek":
+            image = TechGeek;
+            break;
+          default:
+            break;
+        }
+
+        result.push({
+          id: element.id,
+          name: element.name,
+          logo: image,
+          link: element.link,
         });
-    
-        return Shuffle(result);
-      };
+      });
+
+      return Shuffle(result);
+    };
 
     async function loadColunistas() {
       const { data } = await api.get("/categories?parent=36005");
@@ -66,21 +66,21 @@ const Columnists = () => {
   }, [setColunistas]);
 
   return (
-    <Row>
+    <Container>
       <SectionTitle value={"Colunistas"} color={(props) => props.theme.red} />
       <LogoRow>
         {colunistas &&
-            colunistas.length > 0 &&
-            colunistas.map((coluna) => (
+          colunistas.length > 0 &&
+          colunistas.map((coluna) => (
             <ColunaContainer key={coluna.id}>
-                <a href={coluna.link}>
-                <img src={coluna.logo} alt={coluna.name}/>
+              <a href={coluna.link}>
+                <img src={coluna.logo} alt={coluna.name} />
                 <p>{coluna.name}</p>
-                </a>
+              </a>
             </ColunaContainer>
-            ))}
-        </LogoRow>
-    </Row>
+          ))}
+      </LogoRow>
+    </Container>
   );
 };
 
--- src/pages/Home/components/columnists/styled.js
+++ src/pages/Home/components/columnists/styled.js
@@ -1,42 +1,45 @@
-import styled from 'styled-components';
-import { Row } from 'react-flexbox-grid';
+import styled from "styled-components";
+import { Row } from "react-flexbox-grid";
 
 export const Container = styled.div`
-    max-width: 1280px;
-    margin: 20px 0;
+  max-width: 1280px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  padding-top: 30px;
 `;
 
 export const ColunaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 200px;
+
+  a {
+    text-decoration: none;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
-    max-width: 200px;
+    color: black;
+    font-size: 18px;
+  }
 
-    a {
-        text-decoration: none;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        color: black;
-        font-size: 18px;
-    }
+  img {
+    margin: 10px;
+    border: 2px solid ${(props) => props.theme.gray};
+    border-radius: 90px;
+    max-width: 180px;
+    max-height: 180px;
+  }
 
-    img {
-        margin: 10px;
-        border: 2px solid ${(props) => props.theme.gray};
-        border-radius: 90px;
-        max-width: 180px;
-        max-height: 180px;
-    }
-
-    p {
-        font-weight: 600;
-    }
+  p {
+    font-weight: 600;
+  }
 `;
 
 export const LogoRow = styled(Row)`
-    justify-content: space-between;
-    max-width: 1280px;
-    margin: 0 auto !important;
+  justify-content: space-between;
+  max-width: 1280px;
+  margin: 0 auto !important;
 `;
